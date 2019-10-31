f9519b9cd2873734527a25bc9b9313d50b2faacd
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2019-10-31 14:18:04 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,16 @@ setup(
     description="PyMca components for automated data processing with Zocalo at Diamond Light Source",
     author="Tom Schoonjans",
     author_email="Tom.Schoonjans@diamond.ac.uk",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
     packages=["pymca_zocalo"],
     install_requires=["workflows>=1.7", "zocalo", "setuptools", "PyMca5"],
     entry_points={
