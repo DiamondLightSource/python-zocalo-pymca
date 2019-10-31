2ef4eb05c3817824b753561bda9be4dac504338c
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2019-10-31 08:20:19 +0000

    Add Travis-CI support

--- setup.py
+++ setup.py
@@ -1,22 +1,20 @@
 from setuptools import setup
 
-# these lines allow the version to be specified in Makefile.private
-import os
-
-version = os.environ.get("MODULEVER", "0.0")
-
 setup(
     name="pymca-zocalo",
-    version=version,
-    description="Module",
+    version="0.1.0",
+    description="PyMca components for automated data processing with Zocalo at Diamond Light Source",
     author="Tom Schoonjans",
     author_email="Tom.Schoonjans@diamond.ac.uk",
     packages=["pymca_zocalo"],
-    install_requires=["workflows>=1.7", "zocalo", "procrunner", "setuptools", "PyMca5"],
+    install_requires=["workflows>=1.7", "zocalo", "setuptools", "PyMca5"],
     entry_points={
         "workflows.services": [
             "DLSPyMcaFitter = pymca_zocalo:DLSPyMcaFitter",
         ],
     },
     zip_safe=False,
+    license="BSD license",
+    test_suite="tests",
+    url="https://github.com/DiamondLightSource/python-zocalo-pymca",
 )
