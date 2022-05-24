cfd187ccd08023e1c5b8b80ecf949e09ebe7ad2e
Richard Gildea <richard.gildea@diamond.ac.uk>
2022-05-24 09:52:46 +0100

    Bump version: 0.2.0 → 0.2.1

--- pymca_zocalo/__init__.py
+++ pymca_zocalo/__init__.py
@@ -1,4 +1,4 @@
 from .pymca_fitter import DLSPyMcaFitter
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pymca-zocalo",
-    version="0.2.0",
+    version="0.2.1",
     description="PyMca components for automated data processing with Zocalo at Diamond Light Source",
     author="Tom Schoonjans",
     author_email="DataAnalysis@diamond.ac.uk",
