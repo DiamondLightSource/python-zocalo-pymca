96059d9f47dbcbf10220ec7541b51c964b8e90bf
Phil Blowey <philip.blowey@diamond.ac.uk>
2024-09-09 16:24:22 +0100

    Add tests for mca and dat files and remove unused tests

--- tests/test_internals.py
+++ tests/test_internals.py
@@ -6,25 +6,13 @@ import pymca_zocalo.internals as internals
 from pytest import fixture, raises
 
 
-@fixture(scope="session")
-def mock_visit_directory(tmp_path_factory):
-    base_dir = tmp_path_factory.mktemp("base_dir")
-    # Note, this works because base_dir has four levels, putting the visit at the 5th level (matching /dls structure)
-    custom_dir = base_dir / "nt37183-3"
+@fixture
+def mock_visit_directory(tmp_path):
+    # Note, this works because tmp_path has four levels, putting the visit at the 5th level (matching /dls structure)
+    custom_dir = tmp_path / "nt37183-3"
     custom_dir.mkdir(parents=True)
-    return custom_dir
-
-
-# @fixture
-# def mock_cfg_file(monkeypatch, tmp_path):
-
-#     temp_file = tmp_path / "pymca_new.cfg"
-#     shutil.copyfile(Path("tests/pymca_new.cfg"), temp_file)
-
-#     def mock_get_file_path():
-#         return str(temp_file)
-#     monkeypatch.setattr('/dls_sw/i03/software/pymca/pymca_new.cfg', mock_get_file_path)
-#     return temp_file
+    yield custom_dir
+    shutil.rmtree(custom_dir)
 
 
 def test_GIVEN_examplar_data_in_h5file_and_cfg_file_WHEN_run_auto_pymca_called_THEN_produces_expected_files(
@@ -32,10 +20,7 @@ def test_GIVEN_examplar_data_in_h5file_and_cfg_file_WHEN_run_auto_pymca_called_T
 ):
     test_data_file = Path("tests/sample_data.h5")
     temp_input_file = mock_visit_directory / test_data_file.name
-    test_config_file = Path("tests/pymca_new.cfg")
-    temp_config_file = mock_visit_directory / test_config_file.name
     shutil.copyfile(test_data_file, temp_input_file)
-    shutil.copyfile(test_config_file, temp_config_file)
     internals.run_auto_pymca(
         str(temp_input_file),
         "0.0",
@@ -56,31 +41,70 @@ def test_GIVEN_examplar_data_in_h5file_and_cfg_file_WHEN_run_auto_pymca_called_T
         assert file.is_file(), f"File '{file.name}' not written"
 
 
-# def test_GIVEN_examplar_data_in_h5file_WHEN_run_auto_pymca_called_THEN_produces_expected_files(mock_visit_directory, mock_cfg_file):
-#     test_data_file = Path("tests/sample_data.h5")
-#     temp_input_file = mock_visit_directory / test_data_file.name
-#     test_config_file =Path("tests/pymca_new.cfg")
-#     temp_config_file = mock_visit_directory / test_config_file.name
-#     shutil.copyfile(test_data_file, temp_input_file)
-#     shutil.copyfile(test_config_file, temp_config_file)
-#     internals.run_auto_pymca(
-#         str(temp_input_file),
-#         "0.0",
-#         "0.8",
-#         "(0, 0, 0)",
-#         "1.0",
-#         18000.0,
-#     )
-#     tmp_file_basename = Path(temp_input_file).stem
-#     output_mca = mock_visit_directory / f"{tmp_file_basename}.mca"
-#     output_html = mock_visit_directory / f"{tmp_file_basename}.html"
-#     output_png = mock_visit_directory / f"{tmp_file_basename}.png"
-#     pymca_dir = mock_visit_directory / "processed" / "pymca"
-#     output_fit = pymca_dir / "out" / f"{test_data_file.stem}.h5"
-#     output_results = pymca_dir / f"{test_data_file.stem}.results.dat"
+def test_GIVEN_examplar_data_in_mca_format_WHEN_run_auto_pymca_called_THEN_produces_expected_files(
+    mock_visit_directory,
+):
+    test_mca_file = Path("tests/sample_data.mca")
+    test_dat_file = Path("tests/sample_data.dat")
+    test_cfg_file = Path("tests/pymca_new.cfg")
+    temp_mca_file = mock_visit_directory / test_mca_file.name
+    temp_dat_file = mock_visit_directory / test_dat_file.name
+    temp_cfg_file = mock_visit_directory / test_cfg_file.name
+    shutil.copyfile(test_mca_file, temp_mca_file)
+    shutil.copyfile(test_dat_file, temp_dat_file)
+    shutil.copyfile(test_cfg_file, temp_cfg_file)
+    internals.run_auto_pymca(
+        str(temp_mca_file),
+        "0.0",
+        "0.4",
+        "(-1309, -227.0, 230.0)",
+        "1.0",
+        18000.0,
+        src_cfg_file=temp_cfg_file,
+    )
+    tmp_file_basename = Path(temp_mca_file).stem
+    output_mca = mock_visit_directory / f"{tmp_file_basename}.mca"
+    output_html = mock_visit_directory / f"{tmp_file_basename}.html"
+    output_png = mock_visit_directory / f"{tmp_file_basename}.png"
+    pymca_dir = mock_visit_directory / "processed" / "pymca"
+    output_fit = pymca_dir / "out" / f"{tmp_file_basename}.h5"
+    output_results = pymca_dir / f"{tmp_file_basename}.results.dat"
 
-#     for file in [output_mca, output_html, output_png, output_fit, output_results]:
-#         assert file.is_file(), f"File '{file.name}' not written"
+    for file in [output_mca, output_html, output_png, output_fit, output_results]:
+        assert file.is_file(), f"File '{file.name}' not written"
+
+
+def test_GIVEN_examplar_data_in_dat_format_WHEN_run_auto_pymca_called_THEN_produces_expected_files(
+    mock_visit_directory,
+):
+    test_mca_file = Path("tests/sample_data.mca")
+    test_dat_file = Path("tests/sample_data.dat")
+    test_cfg_file = Path("tests/pymca_new.cfg")
+    temp_mca_file = mock_visit_directory / test_mca_file.name
+    temp_dat_file = mock_visit_directory / test_dat_file.name
+    temp_cfg_file = mock_visit_directory / test_cfg_file.name
+    shutil.copyfile(test_mca_file, temp_mca_file)
+    shutil.copyfile(test_dat_file, temp_dat_file)
+    shutil.copyfile(test_cfg_file, temp_cfg_file)
+    internals.run_auto_pymca(
+        str(temp_dat_file),
+        "0.0",
+        "0.4",
+        "(-1309, -227.0, 230.0)",
+        "1.0",
+        18000.0,
+        src_cfg_file=temp_cfg_file,
+    )
+    tmp_file_basename = Path(temp_mca_file).stem
+    output_mca = mock_visit_directory / f"{tmp_file_basename}.mca"
+    output_html = mock_visit_directory / f"{tmp_file_basename}.html"
+    output_png = mock_visit_directory / f"{tmp_file_basename}.png"
+    pymca_dir = mock_visit_directory / "processed" / "pymca"
+    output_fit = pymca_dir / "out" / f"{tmp_file_basename}.h5"
+    output_results = pymca_dir / f"{tmp_file_basename}.results.dat"
+
+    for file in [output_mca, output_html, output_png, output_fit, output_results]:
+        assert file.is_file(), f"File '{file.name}' not written"
 
 
 def test_GIVEN_fitted_peaks_WHEN_call_parse_raw_fluoro_THEN_get_expected_output():
@@ -170,44 +194,3 @@ def test_GIVEN_unsuitable_cutoff_WHEN_call_parse_raw_fluoro_THEN_raise_ValueErro
         internals.parse_raw_fluoro(
             channel_energy, channel_counts, beam_energy, peaks, offset
         )
-
-
-# @patch("pymca_zocalo.internals.open")
-# def test_GIVEN_input_file_not_found_WHEN_find_cut_off_energy_called_THEN_raises_exception(
-#     mock_open,
-# ):
-#     mock_open.side_effect = FileNotFoundError()
-#     with raises(FileNotFoundError):
-#         find_cut_off_energy("test_file_name", 0)
-
-
-# @patch("pymca_zocalo.internals.open", new_callable=mock_open, read_data="1 0")
-# def test_GIVEN_energy_of_1_and_cutoff_of_0_WHEN_find_cut_off_energy_called_THEN_returns_1(
-#     mock_file,
-# ):
-#     line_of_greater_energy = find_cut_off_energy("test_file_name", 0)
-#     assert line_of_greater_energy == 1
-
-
-# @patch("pymca_zocalo.internals.open")
-# def test_different_energies(mock_file):
-#     test_data = "0 1\n5 10\n"
-#     mock_open(mock_file, read_data=test_data)
-#     line_of_greater_energy = find_cut_off_energy("test_file_name", 2)
-#     assert line_of_greater_energy == 1
-
-
-# @patch("pymca_zocalo.internals.open")
-# def test_file_has_non_floats_different_energies(mock_file):
-#     test_data = "hyadasd 1\n5 10\n"
-#     mock_open(mock_file, read_data=test_data)
-#     line_of_greater_energy = find_cut_off_energy("test_file_name", 2)
-#     assert line_of_greater_energy == 1
-
-
-# @patch("pymca_zocalo.internals.open")
-# def test_file_nothing_in_then_gives_none(mock_file):
-#     test_data = ""
-#     mock_open(mock_file, read_data=test_data)
-#     line_of_greater_energy = find_cut_off_energy("test_file_name", 2)
-#     assert line_of_greater_energy is None
